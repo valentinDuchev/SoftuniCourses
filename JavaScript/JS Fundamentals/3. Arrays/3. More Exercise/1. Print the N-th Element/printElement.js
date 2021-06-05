71eb00eb170f90523eab41c27af2365924fdb120
Valentin Duchev <valentinducev@gmail.com>
2021-06-05 22:58:58 +0300

    Added one exercise to Arrays More Exercise

---
+++ JavaScript/JS Fundamentals/3. Arrays/3. More Exercise/1. Print the N-th Element/printElement.js
@@ -0,0 +1,9 @@
+function solve (input) {
+    let step = Number(input.pop());
+    let newArr = [];
+    for (let i = 0; i<input.length; i+=step) {
+        newArr.push(input[i])
+    }
+    console.log(newArr.join(' '))
+}
+solve (['5', '20', '31', '4', '20', '2'])
