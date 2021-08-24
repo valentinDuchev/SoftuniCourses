12ed9ce5ebd44992257a539ca3a7262074a85435
Valentin Duchev <valentinducev@gmail.com>
2021-08-24 13:13:15 +0300

    Replaced ex.1 Real Exam

--- JavaScript/JS Fundamentals/11. Real Exam/1. Problem1/problem01.js
+++ JavaScript/JS Fundamentals/11. Real Exam/1. Problem1/problem01.js
@@ -1,85 +1,87 @@
-function solve (input) {
-    
-    let message = input.shift();
-    let messageLength = Number(message.length);
-    
-    while (input[0] != 'Finish' ) {
-        let token = input.shift().split(' ');
-        let command = token[0];
-        
+function solve(input) {
 
-        if (command === 'Replace') {
-            let toReplace = token[1];
-            let replacement = token[2];
-            let messageArr = message.split(toReplace)
-            message = messageArr.join(replacement)
-            console.log(message)
-        } else if (command === 'Make') {
-            if (token[1] === 'Upper') {
-                let messageArr = message.toUpperCase();
-                message = messageArr;
-                console.log(message)
-            } else if (token[1] === 'Lower') {
-                let messageArr = message.toLowerCase();
-                message = messageArr;
-                console.log(message)
-            }
-        } else if (command === 'Check') {
-            if (message.includes(token[1])){
-                console.log(`Message contains ${token[1]}`);
-            } else {
-                console.log(`Message doesn't contain ${token[1]}`);
-            }
-        } else if (command === 'Sum') {
-            if (Number(token[1])>=0 && Number(token[1]<=messageLength)) {
-                if (Number(token[2])>=0 && Number(token[2])<=messageLength) {
-                    let messageArr = message.substring(Number(token[1]), Number(token[2])).split('');
-                    let result = 0;
-                    for (let element of messageArr) {
-                        result += messageArr.join('').charCodeAt(element)
-                    } console.log(result)
-                    
-                } else {
-                    console.log('Invalid indeces!')
-                }
-            } else {
-                console.log('Invalid indeces!')
-            }
-        } else if (command === 'Cut') {
-            if (Number(token[1])>=0 && Number(token[1]<=messageLength)) {
-                if (Number(token[2])>=0 && Number(token[2])<=messageLength) {
-                    let left = message.substring(0, Number(token[1]));
-                    let rigth = message.substring(Number([token[2]])+1);
-                    message = left + rigth;
-                    //message = messageArr;
-                    console.log(message)
-                } else {
-                    console.log('Invalid indeces!')
-                }
-            } else {
-                console.log('Invalid indeces!')
-            }
+    let word = input.shift();
+
+
+    while (input[0] !== 'Play') {
+        let token = input.shift().split(' ')
+        let command = token[0];
+
+        if (command === 'Move') {
+            let index = token[1];
+            let wordAsArr = word.split('')
+            let toMove = wordAsArr.splice(index, 1);
+            wordAsArr.push(toMove[0]);
+            word = wordAsArr.join('')
+
         }
+
+        else if (command === 'Insert') {
+            let index = token[2];
+            let left = word.substring(0, index);
+            let right = word.substring(index)
+            word = left + ' ' + right;
+
+        }
+
+        else if (command === 'Reverse') {
+            let toReverse = token[1];
+            if (word.includes(toReverse)) {
+                let newWord = word.replace(toReverse, '');
+                let toReverseArr = toReverse.split('').reverse()
+                word = newWord + toReverseArr.join('')
+            }
+
+        }
+
+        else if (command === 'Exchange') {
+            let newLetters = token[2];
+            let newLettersLength = Number(newLetters.length);
+            let first = word.substring(0, newLettersLength);
+            let second = word.substring(newLettersLength);
+            word = newLetters + second;
+            let wordAsArr = word.split('');
+            let newArr = [];
+            for (let i = 0; i < wordAsArr.length; i++) {
+                newArr.push(wordAsArr[i])
+                newArr.push(' ')
+            }
+            console.log(newArr.join(''))
+            return;
+        }
+
+        else if (command === 'Pass') {
+            
+                let wordAsArr = word.split('')
+                let newArr = [];
+                for (let i = 0; i < wordAsArr.length; i++) {
+                    newArr.push(wordAsArr[i])
+                    newArr.push(' ')
+                }
+                console.log(newArr.join(''))
+                return;
+            }
         
+    }
+    if (input[0] === 'Play') {
+        let wordAsArr = word.split(' ');
+        console.log(`You are playing with the word ${wordAsArr[0]}.`)
     }
 
 
-
 }
-solve ((["ILikeSoftUni",
-"Replace I We",
-"Make Upper",
-"Check SoftUni",
-"Sum 1 4",
-"Cut 1 4",
-"Finish"]))
+solve(['XCOBEKA',
+    'Move 1',
+    'Reverse EKAC',
+    'Reverse XOB',
+    'Insert Space 4',
+    'Pass it',
+    'Play'])
 
-console.log('---')
-
-solve((["HappyNameDay",
-"Replace p r",
-"Make Lower",
-"Cut 2 23",
-"Sum -2 2",
-"Finish"])
-)
+console.log('--')
+solve(['BNGOOGG',
+    'Move 2',
+    'Reverse NG',
+    'Insert Space 5',
+    'Exchange Tiles REA',
+    'Play'])
