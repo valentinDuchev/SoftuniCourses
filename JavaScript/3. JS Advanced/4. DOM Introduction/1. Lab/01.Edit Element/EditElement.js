5883d87c89bb2b01a05f962930e7ccf628ec3c90
Valentin Duchev <valentinducev@gmail.com>
2021-09-30 18:36:06 +0300

    Added ex.1 DOM Lab

--- JavaScript/3. JS Advanced/4. DOM Introduction/1. Lab/01.Edit Element/EditElement.js
+++ JavaScript/3. JS Advanced/4. DOM Introduction/1. Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,9 @@
 function editElement() {
+
+    function editElement(ref, match, replacer) {
+        const matcher = new RegExp(match, 'g');
+        const result = ref.textContent.replace(matcher, replacer);
+        ref.textContent = result;
+    }
     
 }
