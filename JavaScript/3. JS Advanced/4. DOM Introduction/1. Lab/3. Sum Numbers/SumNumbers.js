5c941d599c51d0e149b8f38ec44b1a39bdf48c26
Valentin Duchev <valentinducev@gmail.com>
2021-10-02 18:39:15 +0300

    Added ex.3 dom lab

--- JavaScript/3. JS Advanced/4. DOM Introduction/1. Lab/3. Sum Numbers/SumNumbers.js
+++ JavaScript/3. JS Advanced/4. DOM Introduction/1. Lab/3. Sum Numbers/SumNumbers.js
@@ -1,3 +1,12 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    function calc() {
+
+        let first = Number(document.getElementById('num1').value);
+
+        let second = Number(document.getElementById('num2').value);
+
+        let result = first + second;
+
+        document.getElementById('sum').value = result;
+    }
 }
