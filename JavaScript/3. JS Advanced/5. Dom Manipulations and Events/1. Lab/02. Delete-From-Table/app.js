65abd074b74a223804667a703608b12ca3fec042
Valentin Duchev <valentinducev@gmail.com>
2021-10-12 19:06:45 +0300

    added ex.2

--- JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/02. Delete-From-Table/app.js
+++ JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    function deleteByEmail() {
+
+        let input = document.getElementsByName('email')[0].value;
+        let emails = [...document.querySelectorAll('tbody>tr')];
+        let result = document.getElementById('result');
+        for (let i = 0; i < emails.length; i++) {
+            if (input == (emails[i].children[1].textContent)) {
+                emails[i].remove();
+                result.textContent = 'Deleted.';
+            } else {
+                result.textContent = 'Not found.';
+            }
+        }
+
+    }
 }
