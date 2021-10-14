6950896593df3a650be041e6b969e302d68099b4
Valentin Duchev <valentinducev@gmail.com>
2021-10-14 19:08:15 +0300

    added new

--- JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/03. Add-Delete/app.js
+++ JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    function addItem() {
+        let input = document.getElementById('newItemText');
+        let list = document.getElementById('items');
+        let newItem = document.createElement('li');
+        newItem.textContent = input.value;
+        list.appendChild(newItem)
+        input.value = '';
+
+        let deleteBtn = createElement('a', '[Delete]');
+        deleteBtn.href = '#';
+        deleteBtn.addEventListener('click', (ev) => {
+            newItem.remove();
+        });
+        newItem.appendChild(deleteBtn)
+
+        function createElement(type, content) {
+            let element = document.createElement(type);
+            element.textContent = content;
+            return element;
+        }
+
+    }
 }
