8345968bbba7fbfb035ddf4c7325ef81e125ec86
Valentin Duchev <valentinducev@gmail.com>
2021-10-15 19:10:28 +0300

    Added

--- JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/05. Highlight-Active/app.js
+++ JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    function focused() {
+        let elements = [...document.querySelectorAll('input')];
+
+        for (let element of elements) {
+            element.addEventListener('focus', onFocus);
+            element.addEventListener('blur', onBlur);
+        }
+
+        function onFocus(ev) {
+            ev.target.parentNode.className = 'focused';
+        }
+
+        function onBlur(ev) {
+            ev.target.parentNode.className = '';
+        }
+    }
 }
