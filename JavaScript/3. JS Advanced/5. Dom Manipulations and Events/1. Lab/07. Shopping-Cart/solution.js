9066af74e748c63aa60c197f8119ec8069e85278
Valentin Duchev <valentinducev@gmail.com>
2021-10-17 19:14:13 +0300

    Added 7.ex DOM Manip Lab

--- JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/07. Shopping-Cart/solution.js
+++ JavaScript/3. JS Advanced/5. Dom Manipulations and Events/1. Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+   function solve() {
+      let products = [...document.querySelectorAll('.product')];
+      let textArea = document.querySelector('textarea');
+
+      let finalPrice = 0;
+      let finalCart = [];
+
+      for (let i of products) {
+         let addBtn = i.children[2];
+         addBtn.addEventListener('click', clickHandler);
+         function clickHandler(event) {
+            textArea.textContent += `Added ${i.children[1].children[0].textContent} for ${i.children[3].textContent} to the cart.\n`
+            finalPrice += Number(i.children[3].textContent);
+            if (finalCart.includes(i.children[1].children[0].textContent) === false) {
+               finalCart.push(i.children[1].children[0].textContent)
+            }
+         }
+      }
+      let chekoutBtn = document.querySelector('.checkout')
+      chekoutBtn.addEventListener('click', checkoutBtnClickHandler);
+
+      function checkoutBtnClickHandler(event) {
+         textArea.textContent += `You bought ${finalCart.join(', ')} for ${finalPrice.toFixed(2)}.\n`
+      }
+   }
 }
