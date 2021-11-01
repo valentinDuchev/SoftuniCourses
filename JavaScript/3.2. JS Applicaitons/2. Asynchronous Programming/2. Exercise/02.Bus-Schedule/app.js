f3e35d29e791a88aa0ba3310e9cc8f7193cb471b
Valentin Duchev <valentinducev@gmail.com>
2021-11-01 23:46:48 +0200

    Added ex.2 Asynchronous Programming Exercise

--- JavaScript/3.2. JS Applicaitons/2. Asynchronous Programming/2. Exercise/02.Bus-Schedule/app.js
+++ JavaScript/3.2. JS Applicaitons/2. Asynchronous Programming/2. Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,42 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const textArea = document.getElementById('info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    let currentId = 'depot';
+
+    async function depart() {
+        try {
+            let url = `http://localhost:3030/jsonstore/bus/schedule/${currentId}`
+            const response = await fetch (url)
+            if (response.ok == false) {
+                throw new Error (`${response.status} ${response.statusText}`);
+            }
+            const data = await response.json();
+            textArea.textContent = `Next stop ${data.name}`
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+        try {
+            let url = `http://localhost:3030/jsonstore/bus/schedule/${currentId}`
+            const response = await fetch (url);
+            if (response.ok == false) {
+                throw new Error (`${response.status} ${response.statusText}`);
+            }
+            const data = await response.json();
+            departBtn.disabled = false;
+            arriveBtn.disabled = true;
+            
+            textArea.textContent = `Arriving at ${data.name}`
+            currentId = data.next;
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     return {
