5d47213a25f526f67b9f2a33e2e8bf1bfce9c3ee
Valentin Duchev <valentinducev@gmail.com>
2021-11-04 13:07:12 +0200

    Added ex.3 to Asynchronous Programming LAB

--- JavaScript/3.2. JS Applicaitons/2. Asynchronous Programming/1. Lab/03.Gighub-Commits/app.js
+++ JavaScript/3.2. JS Applicaitons/2. Asynchronous Programming/1. Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,37 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+   const username = document.getElementById('username').value
+   const repo = document.getElementById('repo').value
+   const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+   let commits = document.getElementById('commits');
+
+   fetch (url) 
+      .then (response => {
+         if (response.ok == false) {
+            throw new Error (`${response.status} ${response.statusText}`)
+         }
+         return response.json();
+      })
+      .then (handleResponse)
+      .catch(handleError);
+
+
+      function handleResponse (data) {
+         commits.textContent = '';
+         for (let element of data) {
+            let newLi = document.createElement('li');
+            newLi.textContent = `${element.commit.author.name}: ${element.commit.message}`;
+            commits.appendChild(newLi);
+            //console.log(element.commit.author.name)
+            //console.log(element.commit.message)
+         }
+      }
+
+      function handleError (error) {
+         commits.textContent = '';
+         commits.textContent = error.message
+
+
+      }
+
 }
